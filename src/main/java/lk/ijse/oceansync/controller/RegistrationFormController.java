2eb4adc229000a33324fbd32a6704d228557ccec
Charith Harsha <charithharshajayashan123@gmail.com>
2024-04-15 15:53:03 +0530

    create backend in RegistrationFormController

--- src/main/java/lk/ijse/oceansync/controller/RegistrationFormController.java
+++ src/main/java/lk/ijse/oceansync/controller/RegistrationFormController.java
@@ -1,4 +1,50 @@
 package lk.ijse.oceansync.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import lk.ijse.oceansync.db.DbConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class RegistrationFormController {
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtPw;
+
+    @FXML
+    private TextField txtUserId;
+
+    @FXML
+    private void btnRegisterOnAction(ActionEvent event) {
+        String userId = txtUserId.getText();
+        String name = txtName.getText();
+        String pw = txtPw.getText();
+
+        saveUser(userId, name, pw);
+    }
+
+    private void saveUser(String userId, String name, String pw) {
+        try {
+            String sql = "INSERT INTO user VALUES(?, ?, ?)";
+
+            Connection connection = DbConnection.getInstance().getConnection();
+            PreparedStatement pstm = connection.prepareStatement(sql);
+            pstm.setObject(1, name);
+            pstm.setObject(2, userId);
+            pstm.setObject(3, pw);
+
+            if(pstm.executeUpdate() > 0) {
+                new Alert(Alert.AlertType.CONFIRMATION, "user saved!").show();
+            }
+        } catch (SQLException e) {
+            new Alert(Alert.AlertType.ERROR, "something happend!").show();
+        }
+    }
 }
