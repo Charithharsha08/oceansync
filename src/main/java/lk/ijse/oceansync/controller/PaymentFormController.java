fa1343c120353ab210b17bf49d1f1edbfa40c59d
Charith Harsha <charithharshajayashan123@gmail.com>
2024-04-29 08:51:43 +0530

    create loading form controller

--- src/main/java/lk/ijse/oceansync/controller/PaymentFormController.java
+++ src/main/java/lk/ijse/oceansync/controller/PaymentFormController.java
@@ -1,4 +1,148 @@
 package lk.ijse.oceansync.controller;
 
+import com.jfoenix.controls.JFXComboBox;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class PaymentFormController {
+
+    @FXML
+    private JFXComboBox<?> cmbSelectedActivity;
+
+    @FXML
+    private JFXComboBox<?> cmbSelectedCource;
+
+    @FXML
+    private JFXComboBox<?> cmbStock;
+
+    @FXML
+    private JFXComboBox<?> cmbType;
+
+    @FXML
+    private TableColumn<?, ?> colAction;
+
+    @FXML
+    private TableColumn<?, ?> colActivityName;
+
+    @FXML
+    private TableColumn<?, ?> colAmount;
+
+    @FXML
+    private TableColumn<?, ?> colCost;
+
+    @FXML
+    private TableColumn<?, ?> colCourceName;
+
+    @FXML
+    private TableColumn<?, ?> colCustomerName;
+
+    @FXML
+    private TableColumn<?, ?> colDiscount;
+
+    @FXML
+    private TableColumn<?, ?> colPaymentId;
+
+    @FXML
+    private TableColumn<?, ?> colQty;
+
+    @FXML
+    private TableColumn<?, ?> colStock;
+
+    @FXML
+    private TableColumn<?, ?> colTotal;
+
+    @FXML
+    private Label lblDate;
+
+    @FXML
+    private Label lblTime;
+
+    @FXML
+    private AnchorPane rootNode;
+
+    @FXML
+    private TextField txtAmount;
+
+    @FXML
+    private TextField txtCustomerId;
+
+    @FXML
+    private TextField txtCustomerName;
+
+    @FXML
+    private TextField txtPaymentId;
+
+    @FXML
+    private TextField txtQty;
+
+    @FXML
+    private TextField txtSearchTel;
+
+    public void initialize() {
+        setCellValueFactory();
+    }
+
+    private void setCellValueFactory() {
+
+    }
+
+    @FXML
+    void btnAddOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnClearOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnExitOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnSaveOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btncreateNewCustomerOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cmbSelectedActivityOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cmbSelectedCourceOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cmbStockOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cmbTypeOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void tblPayment(ActionEvent event) {
+
+    }
+
 }
